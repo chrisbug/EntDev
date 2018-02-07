50dc0f4059035d576da8ef339c9c89178f1f5f51
chrisbug <christopher199348@gmail.com>
2018-02-07 11:21:28 +0000

    added the usermodel and holdings next work on firebase

---
+++ angular-client/src/app/models/user.model.ts
@@ -0,0 +1,6 @@
+import { UserHolding } from './userholding.model';
+
+export class User {
+  email: string;
+  holdings: [UserHolding]; 
+}
---
+++ angular-client/src/app/models/userholding.model.ts
@@ -0,0 +1,9 @@
+export class UserHolding {
+  company : string;
+  symbol : string;
+  purchesPrice: number;
+  dateIn : Date;
+  dateOut : Date;
+  Qty: number;
+
+}
